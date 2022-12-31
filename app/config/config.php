ea735d01e912b79c740168ac2b963e9a0febc90b
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-12-31 11:45:15 +0700

    database wrapper

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+define('BASEURL','http://localhost:8080/public/');
+
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '12345678');
+define('DB_NAME', 'phpmvc');
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,47 +1,24 @@
 <?php
 
 class Mahasiswa_model{
-    // private $mhs = [
-    //     [
-    //         "nama"=>"Wira",
-    //         "nim"=>"12150311480",
-    //         "email"=>"wira@gmail.com",
-    //         "jurusan"=>"Sistem Informasi"
-    //     ],
-    //     [
-    //         "nama"=>"Fardhan",
-    //         "nim"=>"12151234480",
-    //         "email"=>"fardhan@gmail.com",
-    //         "jurusan"=>"Sastra Pertanian"
-    //     ],
-    //     [
-    //         "nama"=>"Agung Hapsah",
-    //         "nim"=>"12150354321",
-    //         "email"=>"agung@gmail.com",
-    //         "jurusan"=>"DKV"
-    //     ]
-    // ];
 
-    private $dbh; // dabase handler
-    private $stmt;
+
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        // data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        try{
-            $this->dbh = new PDO($dsn, 'root', '12345678');
-        }catch(PDOException $e){
-            die($e->getMessage());
-        }
+        $this->db = new Database;
+    }
+    public function getAllMahasiswa(){
+        $this->db->query('SELECT * from ' . $this->table);
+        return $this->db->resultSet();
     }
 
-    public function getAllMahasiswa(){
-        // return $this->mhs;
-        $this->stmt = $this->dbh->prepare('SELECT * from mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function getMahasiswaById($id){
+        $this->db->query('SELECT * FROM' . $this->table . 'WHERE id=:id');
+        $this->db -> bind('id', $id);
+        return $this->db->single();
     }
 }
 ?>
