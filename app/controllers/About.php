b4620e0581dcf596fb955593f726ddaac906644e
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-12-31 09:05:47 +0700

    views

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,15 +1,24 @@
 <?php
 
-class About
+class About extends Controller
 {
 
-    public function index($nama='wira',$pekerjaan='mahasiswa')
+    public function index($nama = 'wira', $pekerjaan = 'mahasiswa', $umur=20)
     {
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header');
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header');
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
