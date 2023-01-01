562ce6154364aee9dc34874c7e680e5c6a8a647a
muhammadkusuma <mwiraadekusuma1@gmail.com>
2023-01-01 07:31:24 +0700

    searching

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -63,4 +63,13 @@ class Mahasiswa extends Controller
             exit;
         }
     }
+
+    public function cari()
+    {
+        $data['judul'] = 'Darftar Mahasiswa';
+        $data['mhs'] = $this->model('Mahasiswa_model')->cariDataMahasiswa();
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/index', $data);
+        $this->view('templates/footer');
+    }
 }
